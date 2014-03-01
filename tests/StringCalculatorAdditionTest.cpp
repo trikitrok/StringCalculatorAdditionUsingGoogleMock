d458727ddd8887e7e99fd9584658de3dea849682
trikitrok <trikitrok@gmail.com>
2014-03-01 18:50:46 +0100

    Created a fixture to remove duplication in tests

--- tests/StringCalculatorAdditionTest.cpp
+++ tests/StringCalculatorAdditionTest.cpp
@@ -4,20 +4,19 @@
 
 using namespace ::testing;
 
-TEST(StringCalculatorAddition, ReturnsZeroWhenItReceivesAnEmptySequence) {
+class StringCalculatorAddition : public Test {
+public:
   StringCalculator stringCalculator;
+};
 
+TEST_F(StringCalculatorAddition, ReturnsZeroWhenItReceivesAnEmptySequence) {
   ASSERT_THAT(stringCalculator.add(""), Eq(0));
 }
 
-TEST(StringCalculatorAddition, ReturnsTheOnlyNumberInSequence) {
-  StringCalculator stringCalculator;
-
+TEST_F(StringCalculatorAddition, ReturnsTheOnlyNumberInSequence) {
   ASSERT_THAT(stringCalculator.add("5"), Eq(5));
 }
 
-TEST(StringCalculatorAddition, AddsNumbersSeparatedByCommas) {
-  StringCalculator stringCalculator;
-
+TEST_F(StringCalculatorAddition, AddsNumbersSeparatedByCommas) {
   ASSERT_THAT(stringCalculator.add("3, 4, 6"), Eq(13));
 }

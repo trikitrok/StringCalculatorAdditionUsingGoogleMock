46b58d221c3077a64104290d572127dbdae3181d
trikitrok <trikitrok@gmail.com>
2014-03-01 18:19:12 +0100

    Separated numbers extraction and numbers addition in two different loops

--- code/StringCalculator.cpp
+++ code/StringCalculator.cpp
@@ -14,13 +14,19 @@ int StringCalculator::add(const std::string & numbersSequence) {
 
   if (numbersSequence.find(",") != std::string::npos) {
 
-    std::vector<std::string> numbers;
-    boost::split(numbers, numbersSequence, boost::is_any_of(","));
+    std::vector<std::string> numbersStrings;
+    boost::split(numbersStrings, numbersSequence, boost::is_any_of(","));
+
+    std::vector<int> numbers;
+
+    for (unsigned int i = 0; i < numbersStrings.size(); ++i) {
+      numbers.push_back(atoi(numbersStrings[i].c_str()));
+    }
 
     int res = 0;
 
     for (unsigned int i = 0; i < numbers.size(); ++i) {
-      res += atoi(numbers[i].c_str());
+      res += numbers[i];
     }
 
     return res;

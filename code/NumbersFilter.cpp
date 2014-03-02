4eb478876ab2cb178df3766277d99db59621979d
trikitrok <trikitrok@gmail.com>
2014-03-02 13:53:35 +0100

    Made numbers-filtering function name less concrete so later I can add more filters without having to change StringCalculator

--- code/NumbersFilter.cpp
+++ code/NumbersFilter.cpp
@@ -8,6 +8,10 @@ NumbersFilter::NumbersFilter()
 NumbersFilter::~NumbersFilter() 
 {}
 
+std::vector<int> NumbersFilter::filter(const std::vector<int> & numbers) const {
+  return ignoreTooBig(numbers);
+}
+
 std::vector<int> NumbersFilter::ignoreTooBig(const std::vector<int> & numbers) const {
   return VectorUtils::filter(numbers, [](int number) { return !(number > 1000); });
 }
--- code/NumbersFilter.h
+++ code/NumbersFilter.h
@@ -8,6 +8,9 @@ public:
   NumbersFilter();
   virtual ~NumbersFilter();
 
+  std::vector<int> filter(const std::vector<int> & numbers) const;
+
+private:
   std::vector<int> ignoreTooBig(const std::vector<int> & numbers) const;
 };
 
